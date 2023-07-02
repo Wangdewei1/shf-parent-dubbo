cc9e7f6869d2003f4d793572503895ec502701e0
wangddwei <1482466683@qq.com>
2023-07-02 20:15:20 +0800

    房源模块 管理用户头像上传 七牛云图片

--- web/web-admin/src/main/java/com/auto/controller/AclAdminController.java
+++ web/web-admin/src/main/java/com/auto/controller/AclAdminController.java
@@ -1,13 +1,18 @@
 package com.auto.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.auto.constant.FileConstant;
 import com.auto.entity.Admin;
 import com.auto.service.AclAdminService;
+import com.auto.util.FileUtil;
+import com.auto.util.QiniuUtils;
 import com.github.pagehelper.PageInfo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.Map;
 
 /**
@@ -22,6 +27,7 @@ public class AclAdminController {
     public static final String PAGE_SUCCESS = "common/successPage";
     private static final String ACCTION_EDIT_USER = "admin/edit";
     private static final String REDIRECT_ADMIN_INDEX = "redirect:/admin";
+    private static final String ACTION_UPLOAD_AVATAR = "admin/upload";
 
     @Reference
     private AclAdminService aclAdminService;
@@ -82,5 +88,49 @@ public class AclAdminController {
     public String deleteAdmin(@PathVariable("id") Long id){
         aclAdminService.delete(id);
         return REDIRECT_ADMIN_INDEX;
+    }
+
+    /**
+     * 上传头像页面
+     *
+     */
+    @GetMapping("/uploadShow/{id}")
+    public String toUploadAvatarPage(@PathVariable("id") Long id,Model model){
+        model.addAttribute("id", id);
+        return ACTION_UPLOAD_AVATAR;
+    }
+
+    /**
+     * 上传头像
+     */
+    @PostMapping("/upload/{id}")
+    public String uploadAvatar(@PathVariable("id") Long id,
+                               @RequestParam("file") MultipartFile multipartFile,
+                               Model model) throws IOException {
+        //1.上传文件的唯一名称
+        String uuidName = FileUtil.getUUIDName(multipartFile.getOriginalFilename());
+
+        //2.设置上传到七牛云中的路径
+        String randomDirPath = "shf/" + FileUtil.getRandomDirPath(FileConstant.DEFAULT_DIR_LEVEL, FileConstant.DEFAULT_DIR_SIZE);
+
+        //3.转变成七牛云的储存路径
+        String qinNiuDirPath = randomDirPath + uuidName;
+
+        //4.加域名的七牛云储存路径
+        String url = QiniuUtils.getUrl(qinNiuDirPath);
+
+        //5.将图片上传到七牛云
+        QiniuUtils.upload2Qiniu(multipartFile.getBytes(), qinNiuDirPath);
+
+        //6.将土图片路径存到数据库
+        Admin admin = aclAdminService.getById(id);
+        admin.setHeadUrl(url);
+        aclAdminService.update(admin);
+
+        model.addAttribute("messagePage", "上传头像成功");
+
+        return PAGE_SUCCESS;
+
+
     }
 }
